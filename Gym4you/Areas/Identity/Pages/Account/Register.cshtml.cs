638a8f59dc7998c534e594c4228d4b3cb54b7d33
Alex Chmara <alexandrapop@op.pl>
2019-06-01 13:14:52 +0200

    Add UserController.

--- Gym4you/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Gym4you/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -66,7 +66,7 @@ namespace Gym4you.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email  };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 
                 if (result.Succeeded)
---
+++ Gym4you/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Gym4you.Data;
+using Gym4you.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Gym4you.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        public UsersController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _userManager.GetUsersInRoleAsync("User"));
+        }
+
+    }
+}
